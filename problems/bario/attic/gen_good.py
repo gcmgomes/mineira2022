804ffbd5fc1bfa61972be886aee06fcd9830b519
gcmgomes <gcmgomes>
2022-04-29 20:12:20 -0300

    bario solution, checker, testgen

--- problems/bario/attic/gen_good.py
+++ problems/bario/attic/gen_good.py
@@ -11,6 +11,8 @@ seed(sys.argv[1])
 MIN=int(sys.argv[2])
 # third argument is the maximum number of blocks
 MAX=int(sys.argv[3])
+# fourth argument is the maximum size of a chunk
+CHUNK=int(sys.argv[4])
 
 n = randint(MIN,MAX)
 output = ''
@@ -18,7 +20,10 @@ c = '.'
 chunk = 0
 while len(output) < n - 1:
     c = '.' if (c == 'x') else 'x'
-    chunk = randint(1, n - 1 - len(output))
+    max_len = min(CHUNK, n - 1 - len(output))
+    if c == '.':
+        max_len = min(max_len, max(chunk-1, 1))
+    chunk = randint(1, max_len)
     ext = ''
     while len(ext) < chunk:
         ext = ext + c
