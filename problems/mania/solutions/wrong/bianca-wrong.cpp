a3afb574a55e55dd3fc427624da18d07f083bb16
BiaMagri <biancalopes@gec.inatel.br>
2022-05-21 16:28:25 -0300

    add wrong solution

--- problems/mania/solutions/wrong/bianca-wrong.cpp
+++ problems/mania/solutions/wrong/bianca-wrong.cpp
@@ -5,16 +5,12 @@ using namespace std;
 
 char check(int g, string s){
     int calc = 0;
-    int maxD = 0;
-    int maxE = 0;
     
     for(int i=0; i<s.size(); i++){
       if(s[i] == 'D') calc += g;
       else calc -= g;
       
-      maxD = max(maxD, calc);
-      maxE = min(maxE, calc);
-      if(maxD+abs(maxE)>=360) return 'S';
+      if(abs(calc)>=360) return 'S';
     }
     
     return 'N';
