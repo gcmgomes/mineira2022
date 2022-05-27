fb028b464f4656c4607e72744ae188a00d63723f
Dami Henrique <dami.henrique@dami.henrique>
2022-05-26 23:04:22 -0300

    Add capivara good solution

--- problems/capivara/solutions/good/dami.cpp
+++ problems/capivara/solutions/good/dami.cpp
@@ -17,7 +17,6 @@
 using namespace std;
 
 int L, C, K;
-vector<vector<int> > m(L, vector<int>(C, 0));
 vector<int> mid;
 int cor[3];
 
@@ -26,13 +25,13 @@ int solve2d(){
 	return L*C - max(cor[0], max(cor[1], cor[2]));
 }
 
-int solve1d(int c){
+int solve1d(int c, vector<int> v){
 	int need = L*C - cor[c];
-	bool meioEhIgual = true;
-	for(int i=0; i<mid.size(); i++){
-		if(mid[i] != c) meioEhIgual = false;
-	}
-	if(meioEhIgual) need += 2;
+	bool pivoEhIgual = true;
+	for(int i=v.size()-K-1; i<=K; i++)
+		if(v[i] != c)
+			pivoEhIgual = false;
+	if(pivoEhIgual) need += 2;
 	return need;
 }
 
@@ -57,16 +56,10 @@ int main() {
 		int minimo = len/2;
 		if(K < minimo || K >= len) cout << -1 << "\n";
 		else{
-			// 2 possiveis mids
-			if(L >= C) {
-				mid.push_back(m[(len-1)/2][0]);
-				if(len%2 == 0) mid.push_back(m[len/2][0]);
-			}
-			else{
-				mid.push_back(m[0][(len-1)/2]);
-				if(len%2 == 0) mid.push_back(m[0][len/2]);
-			}
-			int ans = min(solve1d(0), min(solve1d(1), solve1d(2)));
+			vector<int> v;
+			if(C >= L) v = m[0];
+			else for(int i=0; i<L; i++) v.push_back(m[i][0]);
+			int ans = min(solve1d(0, v), min(solve1d(1, v), solve1d(2, v)));
 			cout << ans << "\n";
 		}
 	}
